1c952e3a950e4fe0f1ddbe5193fabab56ed149c9
rb1t <rb1t.code@gmail.com>
2024-03-11 19:26:39 -0300

    native steam

--- load_mic/remount_mic.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# sleep 5  # Adjust the sleep time as needed
-echo "Rescanning USB devices..."
-sudo sh -c 'echo 1 > /sys/bus/usb/devices/*/remove'
-sleep 2
-sudo sh -c 'echo 1 > /sys/bus/usb/devices/*/authorized'
--- steam_gamemode/steam-gamemode.sh
+++ steam_gamemode/steam-gamemode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gamemoderun steam #uses system libraries
+gamemoderun steam-native #uses system libraries
 #gamemoderun steam-runtime #uses self-contained runtime bunbled with steam
 
